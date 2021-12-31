c76215a7b8a74d27831099bd086707bc7972e914
Server Auto Commit <riazarbi@gmail.com>
2021-12-31 10:20:00 +0000

    add SP500 constituent scraper

---
+++ SP500.R
@@ -0,0 +1,39 @@
+library(tidyquant)
+library(readr)
+library(aws.s3)
+library(stringr)
+library(arrow)
+library(janitor)
+library(data.table)
+library(dplyr)
+
+files.sources = list.files("R", full.names = T)
+sapply(files.sources, source)
+
+s3_key = Sys.getenv("S3_KEY")
+s3_secret = Sys.getenv("S3_SECRET")
+s3_url = Sys.getenv("S3_URL")
+alphavantage_key = Sys.getenv("ALPHAVANTAGE")
+av_api_key(alphavantage_key)
+
+get_bucket(bucket = "datasci-quant",
+           region = "eu-central-1",
+           key = s3_key,
+           secret = s3_secret,
+           verbose = TRUE) %>% rbindlist
+
+tq_index("SP500") %>%
+  update_minio_dataset(prefix = "datasci-quant/tidyquant/sp500", 
+                     s3_key = s3_key,
+                     s3_secret = s3_secret, 
+                     s3_url = s3_url,
+                     verbose = TRUE)
+
+tq_index("SP500") %>% 
+  tq_get(get = "stock.prices") %>% 
+  update_minio_dataset(prefix = "datasci-quant/tidyquant/sp500.prices", 
+                       s3_key = s3_key,
+                       s3_secret = s3_secret, 
+                       s3_url = s3_url,
+                       verbose = TRUE)
+
