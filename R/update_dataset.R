4b86f2dc1e6335fe55487911ce33b922acff8617
Riaz Arbi <riazarbi@users.noreply.github.com>
2022-02-05 16:26:57 +0200

    Add another insistently

--- R/update_dataset.R
+++ R/update_dataset.R
@@ -100,8 +100,8 @@ update_minio_dataset <- function(new_df,
   }
 
   old_df <- tryCatch({
-    open_dataset(dataset_prefix$path("latest")) %>%
-      collect()
+    purrr::insistently(open_dataset(dataset_prefix$path("latest")) %>%
+      collect())
   }, error = function(error_condition) {
     NA
   })
