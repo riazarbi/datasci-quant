f288a51a25700c9593982075392f8314243d92e8
Riaz Arbi <riazarbi@gmail.com>
2022-09-12 12:23:19 +0200

    tidyquant alphavantage name compat

--- echo_universe.R
+++ echo_universe.R
@@ -3,6 +3,7 @@ suppressMessages({
   library(dplyr)
   library(dataversionr)
   library(jsonlite)
+  library(stringr)
   source("set_env.R")
   source_funs <- sapply(list.files("R", full.names = TRUE), source, .GlobalEnv)
 })
@@ -11,5 +12,6 @@ suppressMessages({
 get_diffs(fix_path("tidyquant/sp500/", dest)) %>% 
   pull(symbol) %>% 
   unique %>% .[1:10] %>% 
+  str_replace_all(pattern = "[.]", "-") %>%
   jsonlite::toJSON() %>% 
   write(stdout())
